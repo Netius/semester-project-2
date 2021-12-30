f2cfd8039b88de3120164bcbc6fe10cacf3fa10b
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2021-12-30 22:29:52 +0100

    Fixed error in cart

--- js/utils/cart.js
+++ js/utils/cart.js
@@ -1,9 +1,10 @@
 import { getExistingCart, removeCart } from "./storage.js";
 
-const shoppingCart = getExistingCart();
-const container = document.querySelector(".product-container");
-
 export function renderCart() {
+    // Her er feilen, function blir kalt fra removeCart() men getExistingCart var utenfor.
+    const shoppingCart = getExistingCart();
+    const container = document.querySelector(".product-container");
+
     if (shoppingCart.length === 0) {
         container.innerHTML = `<p>No items in cart</p>`;
     } else {
